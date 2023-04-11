59fe50dd533b3507d564cbab02d8abea4c2ec178
Gustavo-Valdez <86739150+Gustavo-Valdez@users.noreply.github.com>
2023-04-11 17:01:25 -0600

    Prueba de Webhook

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Prueba <code>src/App.js</code> funciono.
+          Prueba <code>src/App.js</code> funciona por favor.
         </p>
         <a
           className="App-link"
