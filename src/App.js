0172a41087d153b33bbb0b8e1b5454b5951d57d6
Gustavo-Valdez <86739150+Gustavo-Valdez@users.noreply.github.com>
2023-04-12 10:55:40 -0600

    si funciona!!!

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Funcion <code>src/App.js</code> completa.
+          Apoco <code>src/App.js</code> ya funciono.
         </p>
         <a
           className="App-link"
