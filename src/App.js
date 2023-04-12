4748289d5da758a0cca74816fefc74dfc389aade
Gustavo-Valdez <86739150+Gustavo-Valdez@users.noreply.github.com>
2023-04-12 17:16:14 -0600

    Ya probando

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Antes <code>src/App.js</code> de probar .
+          Ya a probar .
         </p>
         <a
           className="App-link"
